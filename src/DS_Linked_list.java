7a02f3147ada15315458cfdfb8d05dfd407bdb34
“SujitAmbore” <asujitambore@gmail.com>
2022-10-24 12:59:34 +0800

    Linked List program

--- src/DS_Linked_list.java
+++ src/DS_Linked_list.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DS_Linked_list {
+import java.util.LinkedList;
+
+public class DS_Linked_list {
+
+
+    public static void main(String[] args) {
+        LinkedList<Integer> li = new LinkedList<>();
+        li.add(10);
+        li.add(9);
+        li.add(8);
+        li.add(7);
+        li.add(6);
+        System.out.println(li);
+
+        if (li.getFirst() == li.getLast())
+            System.out.println("same");
+        else
+            System.out.println("different");
+
+
+
+    }
+
 }
